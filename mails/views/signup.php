8e41cd1d01db03ec02a405cc396390cb0a0dd644
vova07 <bazillio07@yandex.ru>
2014-06-05 13:10:11 +0300

    Fix email urls.

--- mails/views/signup.php
+++ mails/views/signup.php
@@ -8,9 +8,7 @@
 use yii\helpers\Html;
 use yii\helpers\Url;
 
-$url = Url::toRoute('/users/guest/activation', [
-    'key' => $model['secure_key']
-]); ?>
+$url = Url::toRoute(['/users/guest/activation', 'key' => $model['secure_key']]); ?>
 <p>Hello <?= Html::encode($model['username']) ?>,</p>
 <p>Follow the link below to activate your account:</p>
 <p><?= Html::a(Html::encode($url), $url) ?></p>
