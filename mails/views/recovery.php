56cda6c85d92cfc939f90846040c6af173d58b85
vova07 <bazillio07@yandex.ru>
2014-06-05 14:56:27 +0300

    Fix recovery url.

--- mails/views/recovery.php
+++ mails/views/recovery.php
@@ -8,7 +8,7 @@
 use yii\helpers\Html;
 use yii\helpers\Url;
 
-$url = Url::toRoute(['/users/guest/recovery', 'key' => $model['secure_key']], true); ?>
+$url = Url::toRoute(['/users/guest/recovery-confirmation', 'key' => $model['secure_key']], true); ?>
 <p>Hello <?= Html::encode($model['username']) ?>,</p>
 <p>Follow the link below to recover your password:</p>
 <p><?= Html::a(Html::encode($url), $url) ?></p>
--- mails/views/ru/recovery.php
+++ mails/views/ru/recovery.php
@@ -8,7 +8,7 @@
 use yii\helpers\Html;
 use yii\helpers\Url;
 
-$url = Url::toRoute(['/users/guest/recovery', 'key' => $model['secure_key']], true); ?>
+$url = Url::toRoute(['/users/guest/recovery-confirmation', 'key' => $model['secure_key']], true); ?>
 <p>Здравствуйте <?= Html::encode($model['username']) ?>!</p>
 <p>Перейдите по ссылке ниже чтобы восстановить пароль:</p>
 <p><?= Html::a(Html::encode($url), $url) ?></p>
